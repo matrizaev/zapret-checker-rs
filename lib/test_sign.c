44f81cf3587bf4640882dbed3c3e024518947ca0
matrizaev <matrizaev@gmail.com>
2024-05-08 01:54:33 +0500

    updated test

--- lib/test_sign.c
+++ lib/test_sign.c
@@ -2,19 +2,21 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "sign.h"
 
 void main() {
     char *data = "test";
     char *PIN = "12345678";
-    char *key_pair_id = "00000000";
+    char *key_pair_id = "7e4a9abc9-403b-";
 
     size_t result_size = 0;
-    uint8_t *result = perform_signing(data, arraysize(data), &result_size, PIN, arraysize(PIN), key_pair_id, arraysize(key_pair_id));
+    uint8_t *result = perform_signing(data, strlen(data), &result_size, PIN, strlen(PIN), key_pair_id, strlen(key_pair_id));
     if (result == NULL || result_size == 0){
         puts("perform_signing failed");
-    }
-    else
+    } else {
         free(result);
+		puts("test passed");
+	}
 }
